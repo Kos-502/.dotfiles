7691b48758aa0eb0411b7c5ffd600ce453b74e83
Kos-502 <designation.kos+github@gmail.com>
2025-02-17 14:35:17 +0200

    Add mason back in

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -25,6 +25,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
+require('mason').setup()
 require('neodev').setup()
 require('lspconfig').lua_ls.setup {
     on_attach = on_attach,
