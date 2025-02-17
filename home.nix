ff06a961cbfa7c7cc28951162a6c4152d0d407d3
Kos-502 <designation.kos+github@gmail.com>
2025-02-17 14:14:58 +0200

    Configure neovim inside home-manager

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   # Enable home manager.
@@ -17,6 +17,83 @@
   # Declare environment variables.
   home.sessionVariables = {
     EDITOR = "nvim";
+  };
+
+  programs.neovim =
+  let
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+  in
+  {
+    enable = true;
+
+    extraPackages = with pkgs; [
+      wl-clipboard
+
+      luajitPackages.lua-lsp
+      rnix-lsp
+    ];
+
+    plugins = with pkgs.vimPlugins; [
+      # Line commenter plugin
+      {
+        plugin = comment-nvim;
+        config = toLua "require(\"Comment\").setup()";
+      }
+
+      # LSP
+      {
+        plugin = nvim-lspconfig;
+        config = toLuaFile ./nvim/plugin/lsp.lua;
+      }
+      
+      # Gruvbox colorscheme
+      {
+        plugin = gruvbox-nvim;
+        config = "colorscheme gruvbox";
+      }
+
+      # Lualine (better vim line)
+      lualine-nvim
+      nvim-web-devicons
+
+      neodev-nvim
+
+      # Code completion
+      {
+        plugin = nvim-cmp;
+        config = toLuaFile ./nvim/plugin/cmp.lua;
+      }
+
+      cmp_luasnip
+      cmp-nvim-lsp
+
+      # Code snippets
+      luasnip
+      friendly-snippets
+
+      # Telescope
+      {
+        plugin = telescope-nvim;
+        config = toLuaFile ./nvim/plugin/telescope.lua;
+      }
+
+      vim-nix
+      
+      # Treesitter
+      (vimPlugins.nvim-treesitter.withPlugins (p: [
+        p.tree-sitter-nix
+        p.tree-sitter-vim
+        p.tree-sitter-bash
+        p.tree-sitter-lua
+        p.tree-sitter-python
+        p.tree-sitter-json
+      ]))
+    ];
+
+    extraLuaConfig = ''
+      ${builtins.readFile ./nvim/options.lua}
+    '';
   };
 
   # ! ------------- CAUTION ------------- ! #
