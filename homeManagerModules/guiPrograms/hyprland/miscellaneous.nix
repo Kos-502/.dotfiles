d7ca29751b0dd1d02e8da2a1eaba9ff1f94c0335
Kos-502 <designation.kos+github@gmail.com>
2025-02-21 01:07:23 +0200

    Set miscellaneous settings

---
+++ homeManagerModules/guiPrograms/hyprland/miscellaneous.nix
@@ -0,0 +1,17 @@
+{ ... }:
+
+{
+  # Set some miscellaneous Hyprland options.
+
+  wayland.windowManager.hyprland.settings = {
+    env = [
+      "XCURSOR_SIZE, 24"
+      "HYPRCURSOR_SIZE, 24"
+    ];
+
+    windowrulev2 = [
+      "suppressevent maximize, class:.*"
+      "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+    ];
+  };
+}
