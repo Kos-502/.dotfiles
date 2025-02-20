e7e9918c21334fecb633f569be6bfd538d8708c3
Kos-502 <designation.kos+github@gmail.com>
2025-02-20 19:22:43 +0200

    Rename the file that imported the alacritty nix modules to default.nix

--- homeManagerModules/guiPrograms/alacritty/default.nix
+++ homeManagerModules/guiPrograms/alacritty/default.nix
@@ -4,18 +4,12 @@
   # ALACRITTY - a modern terminal emulator that comes with sensible defaults,
   # but allows for extensive configuration.
 
+  programs.alacritty.enable = true;
+
   # Import the custom Alacritty nix modules.
   imports = [
     ./appearance.nix
     ./theme.nix
+    ./miscellaneous.nix
   ];
-
-  programs.alacritty = {
-    enable = true;
-
-    settings = {
-      selection.save_to_clipboard = true;
-      mouse.hide_when_typing = true;
-    };
-  };
 }
