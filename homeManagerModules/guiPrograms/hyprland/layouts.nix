826da7212f3e04f7f11468f015272c0f001d618a
Kos-502 <designation.kos+github@gmail.com>
2025-02-21 00:45:10 +0200

    Define the available layouts

---
+++ homeManagerModules/guiPrograms/hyprland/layouts.nix
@@ -0,0 +1,16 @@
+{ ... }:
+
+{
+  # Define the layouts Hyprland can use.
+
+  wayland.windowManager.hyprland.settings = {
+    dwindle = {
+      pseudotile = true;
+      preserve_split = true;
+    };
+
+    master = {
+      new_status = "master";
+    };
+  };
+}
