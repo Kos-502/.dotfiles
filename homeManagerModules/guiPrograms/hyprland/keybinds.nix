1e8b1e39df8d0628df3305e09853d57ff264d1ab
Kos-502 <designation.kos+github@gmail.com>
2025-02-21 00:44:46 +0200

    Define keybinds

--- homeManagerModules/guiPrograms/hyprland/keybinds.nix
+++ homeManagerModules/guiPrograms/hyprland/keybinds.nix
@@ -1,30 +1,69 @@
 { ... }:
 
 {
+  # Define every used keybind.
+
   wayland.windowManager.hyprland.settings = let
+    # The main mod key - WIN key.
     mod = "SUPER";
+
     terminal = "alacritty";
     menu = "rofi -show drun -show-icons";
+    fileManager = "lf";
   in
   {
     bindm = [
-      # Terminal
-      "${mod}, Q, exec, ${terminal}"
+      ### HYPRLAND
+
+      # Exit the current session.
+      "${mod}, M, exit"
+
+      ### WINDOW
       
-      # Window
+      # Actions:
       "${mod}, C, killactive"
       "${mod}, V, togglefloating"
+      "${mod}, mouse:272, movewindow"
+      "${mod}, mouse:273, resizewindow"
 
+      # Dwindle-specific actions:
+      "${mod}, J, togglesplit"
+      "${mod}, P, pseudo"
+
+      # Move to another window.
       "${mod}, left, movefocus, l"
       "${mod}, right, movefocus, r"
       "${mod}, up, movefocus, u"
       "${mod}, down, movefocus, d"
 
-      # Hyprland
-      "${mod}, M, exit"
+      ### APPLICATIONS
 
-      # Menu
+      "${mod}, Q, exec, ${terminal}"
       "${mod}, R, exec, ${menu}"
+      "${mod}, F, exec, ${fileManager}"
+
+      ### WORKSPACES
+
+      # Move to another workspace.
+      "${mod}, 1, workspace, 1"
+      "${mod}, 2, workspace, 2"
+      "${mod}, 3, workspace, 3"
+      "${mod}, 4, workspace, 4"
+      "${mod}, 5, workspace, 5"
+
+      # Move a window to another workspace.
+      "${mod} SHIFT, 1, movetoworkspace, 1"
+      "${mod} SHIFT, 2, movetoworkspace, 2"
+      "${mod} SHIFT, 3, movetoworkspace, 3"
+      "${mod} SHIFT, 4, movetoworkspace, 4"
+      "${mod} SHIFT, 5, movetoworkspace, 5"
+
+      # Special workspace
+      # (A workspace where you can move windows that
+      # you regularly access but don't want to waste
+      # an entire workspace for.)
+      "${mod}, S, togglespecialworkspace, magic"
+      "${mod} SHIFT, S, movetoworkspace, special:magic"
     ];
   };
 }
