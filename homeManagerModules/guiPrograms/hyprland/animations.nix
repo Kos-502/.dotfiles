9039504751ef853fda3ac20223d76997d3e62a81
Kos-502 <designation.kos+github@gmail.com>
2025-02-21 08:44:26 +0200

    Steal someone's animation (hehehehe)

--- homeManagerModules/guiPrograms/hyprland/animations.nix
+++ homeManagerModules/guiPrograms/hyprland/animations.nix
@@ -4,6 +4,33 @@
   # Define Hyprland animations.
 
   wayland.windowManager.hyprland.settings = {
-    animations = { };
+    animations = {
+      enabled = true;
+
+      bezier = "easeOutQuart, 0.190, 0.91, 0.37, 1";
+
+      animation = [
+        "windowsIn, 1, 5, easeOutQuart, popin 0%"
+        "windowsOut, 1, 5, easeOutQuart, popin 60%"
+        "windowsMove, 1, 5, easeOutQuart, popin 60%"
+        "windows, 1, 5, easeOutQuart"
+
+        "layers, 1, 5, easeOutQuart, popin 70%"
+
+        "fadeIn, 1, 5, easeOutQuart"
+        "fadeOut, 1, 5, easeOutQuart"
+        "fadeSwitch, 0, 5, easeOutQuart"
+        "fadeShadow, 0, 5, easeOutQuart"
+        "fadeDim, 1, 5, easeOutQuart"
+        "fadeLayers, 1, 5, easeOutQuart"
+        "fade, 1, 5, easeOutQuart"
+
+        "border, 0, 5, easeOutQuart"
+        "borderangle, 0, 5, easeOutQuart"
+
+        "specialWorkspace, 1, 5, easeOutQuart, slidevert"
+        "workspaces, 1, 5, easeOutQuart, slide"
+      ];
+    };
   };
 }
