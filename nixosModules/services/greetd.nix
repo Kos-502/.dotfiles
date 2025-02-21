279e34aa1a0fc48905d2169826a404b91face45d
Kos-502 <designation.kos+github@gmail.com>
2025-02-21 08:32:54 +0200

    Clean up the greetd service file

--- nixosModules/services/greetd.nix
+++ nixosModules/services/greetd.nix
@@ -10,12 +10,22 @@
     settings = {
       default_session = let
         tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+        width = "50";
         timeFormat = "[ %B %Y, %A %d --- %I:%M %p ]";
         greeting = "Welcome, user!";
+        passwordChar = "#";
         session = "Hyprland";
       in
       {
-        command = "${tuigreet} --time --time-format '${timeFormat}' --greeting '${greeting}' --cmd ${session}";
+        command = ''
+          ${tuigreet} \
+          --width ${width} \
+          --time --time-format "${timeFormat}" \
+          --greeting "${greeting}" \
+          --asterisks --asterisks-char "${passwordChar}" \
+          --cmd ${session}
+        '';
+
         user = "greeter";
       };
     };
