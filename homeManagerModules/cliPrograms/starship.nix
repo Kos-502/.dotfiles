9781210ee1c2a92b7802cca9d7a1f644c6c6c72a
Kos-502 <designation.kos+github@gmail.com>
2025-02-21 09:25:09 +0200

    Implement a simple starship prompt

--- homeManagerModules/cliPrograms/starship.nix
+++ homeManagerModules/cliPrograms/starship.nix
@@ -1,11 +1,23 @@
-{ ... }:
+{ lib, ... }:
 
 {
   programs.starship = {
     enable = true;
 
     settings = {
-      add_newline = false;
+      add_newline = true;
+
+      format = lib.strings.concatStrings [
+        "$directory"
+        "$git_branch"
+        "$git_status"
+
+        "$python"
+        "$jobs"
+        "$cmd_duration"
+
+        "$character"
+      ];
     };
   };
 }
