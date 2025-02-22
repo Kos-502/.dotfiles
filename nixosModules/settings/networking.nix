73594c3c4158c4fbaecbb775769e8faaf6249e22
Kos-502 <designation.kos+github@gmail.com>
2025-02-22 14:22:29 +0200

    Remove the hostName definition from the shared modules

--- nixosModules/settings/networking.nix
+++ nixosModules/settings/networking.nix
@@ -3,8 +3,5 @@ _:
 {
   # NETWORKING - he process of connecting two or more computing devices.
 
-  networking = {
-    hostName = "MUUN";
-    networkmanager.enable = true;
-  };
+  networking.networkmanager.enable = true;
 }
