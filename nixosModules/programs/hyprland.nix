a178d2c57d34fb08207383d7635c6080b627dd52
Kos-502 <designation.kos+github@gmail.com>
2025-02-22 17:07:46 +0200

    Remove different version of hyprland being installed

--- nixosModules/programs/hyprland.nix
+++ nixosModules/programs/hyprland.nix
@@ -1,8 +1,5 @@
-{ pkgs, inputs, ... }:
+{ ... }:
 
 {
-  programs.hyprland = {
-    enable = true;
-    package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-  };
+  programs.hyprland.enable = true;
 }
