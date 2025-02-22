5121a3d9bb7c98ba1d01e55dbaaa5063b92d7181
Kos-502 <designation.kos+github@gmail.com>
2025-02-22 13:23:27 +0200

    Rename the default.nix modules to the name of their program

---
+++ homeManagerModules/guiPrograms/hyprland/hyprland.nix
@@ -0,0 +1,24 @@
+{ config, lib, ... }:
+
+{
+  # HYPRLAND - a 100% independent, dynamic tiling Wayland compositor.
+
+  options = {
+    hyprland.enable =
+      lib.mkEnableOption "enables hyprland";
+  };
+
+  config = lib.mkIf config.hyprland.enable {
+    wayland.windowManager.hyprland.enable = true;
+
+    imports = [
+      ./autostart.nix
+      ./monitor.nix
+      ./appearance.nix
+      ./input.nix
+      ./keybinds.nix
+      ./miscellaneous.nix
+      ./animations.nix
+    ];
+  };
+}
