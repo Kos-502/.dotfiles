63fcc1a12a4a470a1fa65dcd98af33d0ff41ce84
Kos-502 <designation.kos+github@gmail.com>
2025-02-22 19:06:21 +0200

    Clean up the flake config

--- flake.nix
+++ flake.nix
@@ -1,5 +1,18 @@
+#     __ _      _              _     
+#    / _| |__ _| |_____   _ _ (_)_ __
+#   |  _| / _` | / / -_)_| ' \| \ \ /
+#   |_| |_\__,_|_\_\___(_)_||_|_/_\_\
+
 {
-  description = "NixOS config flake";
+  description = "This is Kos-502's NixOS configuration flake.";
+
+  outputs = { nixpkgs, ... } @ inputs:
+  {
+    nixosConfigurations."MUUN" = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [ ./hosts/MUUN/configuration.nix ];
+    };
+  };
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -10,14 +23,5 @@
     };
 
     hyprland.url = "github:hyprwm/Hyprland";
-  };
-
-  outputs = { nixpkgs, ... } @ inputs:
-  {
-    nixosConfigurations."MUUN" = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-
-      modules = [ ./hosts/MUUN/configuration.nix ];
-    };
   };
 }
