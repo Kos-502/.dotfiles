deb49625a5164b43c7215265e15b05814bcde89e
Kos-502 <designation.kos+github@gmail.com>
2025-02-24 23:59:21 +0200

    Separate the prompt related options in a module

---
+++ homeManagerModules/cliPrograms/zsh/prompt.nix
@@ -0,0 +1,20 @@
+#                            _          _     
+#    _ __ _ _ ___ _ __  _ __| |_   _ _ (_)_ __
+#   | '_ \ '_/ _ \ '  \| '_ \  _|_| ' \| \ \ /
+#   | .__/_| \___/_|_|_| .__/\__(_)_||_|_/_\_\
+#   |_|                |_|                    
+
+{ ... }:
+
+{
+  programs.zsh = {
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+
+    ohMyZsh = {
+      enable = true;
+      theme = "robbyrussell";
+    };
+  };
+}
