8f7d015af93053c6035b0a5c6eda5e2e193d3dfa
Kos-502 <designation.kos+github@gmail.com>
2025-02-25 11:26:38 +0200

    Create the main default file for the waybar GUI

---
+++ homeManagerModules/guiPrograms/waybar/default.nix
@@ -0,0 +1,20 @@
+#       _      __           _ _          _     
+#    __| |___ / _|__ _ _  _| | |_   _ _ (_)_ __
+#   / _` / -_)  _/ _` | || | |  _|_| ' \| \ \ /
+#   \__,_\___|_| \__,_|\_,_|_|\__(_)_||_|_/_\_\
+                                           
+# WAYBAR - GTK status bar made specifically for
+#          wlroots compositors and supports Hyprland by default
+
+{ config, lib, ... }:
+
+{
+  options = {
+    waybar.enable =
+      lib.mkEnableOption "enables waybar";
+  };
+
+  config = lib.mkIf config.waybar.enable {
+    programs.waybar.enable = true;
+  };
+}
