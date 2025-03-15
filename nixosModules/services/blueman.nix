6541ea8cccea8ae3071d9d8f5bc77b3c50e14e08
Kos-502 <designation.kos+github@gmail.com>
2025-03-15 16:39:12 +0200

    Enable the blueman service

---
+++ nixosModules/services/blueman.nix
@@ -0,0 +1,9 @@
+#    _    _                                 _     
+#   | |__| |_  _ ___ _ __  __ _ _ _    _ _ (_)_ __
+#   | '_ \ | || / -_) '  \/ _` | ' \ _| ' \| \ \ /
+#   |_.__/_|\_,_\___|_|_|_\__,_|_||_(_)_||_|_/_\_\
+                                              
+# BLUEMAN - a full featured Bluetooth manager
+#           written in Python and using GTK
+
+{ ... }: { services.blueman.enable = true; }
